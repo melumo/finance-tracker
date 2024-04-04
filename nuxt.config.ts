978d5807948b3e9b39a5f039f535a24c5e53516c
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2024-04-04 09:22:34 -0300

    chore: add nuxt fonts
    fix: add missing missing props to @nuxt/fonts module
    
    Revert "chore: add nuxt fonts"
    
    This reverts commit 02698d6b9365fa17e5a77b318604716bccb20138.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,26 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   srcDir: 'src/',
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@nuxt/fonts'],
+  fonts: {
+    provider: 'google',
+    families: [
+      {
+        name: 'Inter',
+        weights: ['400..700'],
+        subsets: [
+          'cyrillic-ext',
+          'cyrillic',
+          'greek-ext',
+          'greek',
+          'vietnamese',
+          'latin-ext',
+          'latin',
+        ],
+      },
+    ],
+    experimental: {
+      addPreloadLinks: true,
+    },
+  },
 })
