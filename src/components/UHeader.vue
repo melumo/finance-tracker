8983851a1c549909b18128707112e28eab080ff6
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2024-04-05 11:30:10 -0300

    feat: add header component

---
+++ src/components/UHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="relative p-4 lg:px-0">
+    <div class="w-full flex justify-between max-w-screen-lg mx-auto">
+      <div class="flex flex-col">
+        <h1 class="text-xl font-bold md:text-2xl">Welcome back, Max</h1>
+        <h2 class="text-sm text-gray-600 dark:text-gray-300 md:text-base">
+          Track, manage, and forecast your finances.
+        </h2>
+      </div>
+      <div>
+        <UAvatar
+          src="https://avatars.githubusercontent.com/u/739984?v=4"
+          alt="Avatar"
+          size="md"
+        />
+      </div>
+    </div>
+  </header>
+</template>
