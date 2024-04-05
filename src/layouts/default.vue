acbc4a0de8de782e91fd3a6141104964b8bfbc20
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2024-04-05 11:31:17 -0300

    feat: add default layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="relative min-h-screen">
+    <UHeader />
+    <main class="px-4 py-8 max-w-screen-lg mx-auto lg:px-0">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style>
+:root {
+  font-family:
+    Inter,
+    system-ui,
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Oxygen,
+    Roboto,
+    Ubuntu,
+    Cantarell,
+    'Open Sans',
+    'Helvetica Neue',
+    sans-serif;
+}
+</style>
