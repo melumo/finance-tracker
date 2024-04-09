a3759c094d20aca580e0ec2fdecffadc201694d3
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2024-04-09 18:51:30 -0300

    feat: ensure amount can be a string

--- src/composables/use-currency.ts
+++ src/composables/use-currency.ts
@@ -1,4 +1,4 @@
-export const useCurrency = (amount: number) => {
+export const useCurrency = (amount: number | string) => {
   const currency = computed(() =>
     amount.toLocaleString('en-US', {
       style: 'currency',
